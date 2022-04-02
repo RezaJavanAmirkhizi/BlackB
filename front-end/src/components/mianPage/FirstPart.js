34cea83d3782006dfa290b40809354db26f6e7af
Reza javan <87618089+rezajavan1380@users.noreply.github.com>
2022-04-03 00:30:17 +0430

    create first part of main page and change some styles

---
+++ front-end/src/components/mianPage/FirstPart.js
@@ -0,0 +1,94 @@
+/* Display posts by relevant and latest and top condition */
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+// import Swiper core and required modules
+import { Autoplay, Pagination, Navigation } from "swiper";
+
+
+const FirstPart = () => {
+	return (
+		<div className="container">
+			<Swiper
+				spaceBetween={30}
+				centeredSlides={true}
+				autoplay={{
+					delay: 2500,
+					disableOnInteraction: false,
+				}}
+				pagination={{
+					clickable: true,
+				}}
+				navigation={false}
+				modules={[Autoplay, Pagination, Navigation]}
+				className="mySwiper"
+			>
+				<SwiperSlide>Slide 1</SwiperSlide>
+				<SwiperSlide>Slide 2</SwiperSlide>
+				<SwiperSlide>Slide 3</SwiperSlide>
+				<SwiperSlide>Slide 4</SwiperSlide>
+				<SwiperSlide>Slide 5</SwiperSlide>
+				<SwiperSlide>Slide 6</SwiperSlide>
+				<SwiperSlide>Slide 7</SwiperSlide>
+				<SwiperSlide>Slide 8</SwiperSlide>
+				<SwiperSlide>Slide 9</SwiperSlide>
+			</Swiper>
+			<Swiper
+				spaceBetween={30}
+				centeredSlides={true}
+				autoplay={{
+					delay: 2500,
+					disableOnInteraction: false,
+				}}
+				pagination={{
+					clickable: true,
+				}}
+				navigation={false}
+				modules={[Autoplay, Pagination, Navigation]}
+				className="mySwiper"
+			>
+				<SwiperSlide>Slide 1</SwiperSlide>
+				<SwiperSlide>Slide 2</SwiperSlide>
+				<SwiperSlide>Slide 3</SwiperSlide>
+				<SwiperSlide>Slide 4</SwiperSlide>
+				<SwiperSlide>Slide 5</SwiperSlide>
+				<SwiperSlide>Slide 6</SwiperSlide>
+				<SwiperSlide>Slide 7</SwiperSlide>
+				<SwiperSlide>Slide 8</SwiperSlide>
+				<SwiperSlide>Slide 9</SwiperSlide>
+			</Swiper>
+			<Swiper
+				spaceBetween={30}
+				centeredSlides={true}
+				autoplay={{
+					delay: 2500,
+					disableOnInteraction: false,
+				}}
+				pagination={{
+					clickable: true,
+				}}
+				navigation={false}
+				modules={[Autoplay, Pagination, Navigation]}
+				className="mySwiper"
+			>
+				<SwiperSlide>Slide 1</SwiperSlide>
+				<SwiperSlide>Slide 2</SwiperSlide>
+				<SwiperSlide>Slide 3</SwiperSlide>
+				<SwiperSlide>Slide 4</SwiperSlide>
+				<SwiperSlide>Slide 5</SwiperSlide>
+				<SwiperSlide>Slide 6</SwiperSlide>
+				<SwiperSlide>Slide 7</SwiperSlide>
+				<SwiperSlide>Slide 8</SwiperSlide>
+				<SwiperSlide>Slide 9</SwiperSlide>
+			</Swiper>
+		</div>
+		
+	);
+};
+
+export default FirstPart;
--- front-end/src/components/mianPage/index.js
+++ front-end/src/components/mianPage/index.js
@@ -1,9 +1,11 @@
 import Header from "./header";
+import FirstPart from "./FirstPart"
 
 const mainPage = () => {
 	return (
 		<>
 			<Header />
+			<FirstPart />
 		</>
 	);
 };
