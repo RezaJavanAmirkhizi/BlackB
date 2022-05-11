58c08cdad299fe1b08db9885fc61d8be2570695b
Reza javan <87618089+rezajavan1380@users.noreply.github.com>
2022-05-11 13:02:32 +0430

    Edit some wrong codes

--- front-end/src/components/account/CreatePost.js
+++ front-end/src/components/account/CreatePost.js
@@ -3,19 +3,19 @@ function CreatePost() {
 		<div className="dashboard-boxes">
 			<h1>Create a new post</h1>
 			<div className="row">
-				<label for="title">Title:</label>
+				<label htmlFor="title">Title:</label>
 				<input id="title" type="text" />
 			</div>
 			<div className="row">
-				<label for="body">Body:</label>
+				<label htmlFor="body">Body:</label>
 				<textarea id="body" type="text" />
 			</div>
 			<div className="row">
-				<label for="picture">Picture:</label>
+				<label htmlFor="picture">Picture:</label>
 				<input id="picture" type="file" />
 			</div>
 			<div className="row">
-				<label for="tags">tags:</label>
+				<label htnlFor="tags">tags:</label>
 				<input id="tags" type="search" />
 			</div>
 			<button className="change">Create</button>
