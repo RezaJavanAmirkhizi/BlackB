38900abba7acb385c25e7778066e90b05c1ddaea
Reza javan <87618089+rezajavan1380@users.noreply.github.com>
2022-05-11 12:55:19 +0430

    Route to dashboard

--- front-end/src/components/App.js
+++ front-end/src/components/App.js
@@ -3,6 +3,7 @@ import MainPage from "./mianPage";
 import { Routes, Route } from "react-router-dom";
 import Login from "./auth/Login";
 import Register from "./auth/Register";
+import Dashboard from "./account/Dashboard"
 
 function App() {
 	return (
@@ -18,6 +19,7 @@ function App() {
 			/>
 			<Route exact path="/login" element={<Login />} />
 			<Route exact path="/register" element={<Register />} />
+			<Route exact path="/dashboard" element={<Dashboard />} />
 		</Routes>
 	);
 }
