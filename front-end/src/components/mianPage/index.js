0c1266f501a37404a14c5fbced102ea1b4bb735e
Reza javan <87618089+rezajavan1380@users.noreply.github.com>
2022-05-11 13:02:09 +0430

    Set authentication for site

--- front-end/src/components/mianPage/index.js
+++ front-end/src/components/mianPage/index.js
@@ -1,15 +1,22 @@
 import Header from "./Header";
 import FirstPart from "./FirstPart";
 import Cards from "./Cards";
+import { useState, useEffect } from "react";
+import jwt_decode from "jwt-decode";
 
-const mainPage = () => {
+const MainPage = () => {
+
+
+	const [auth, setAuth] = useState(localStorage.getItem("access_token")? true : false);
+	const [user, setUser] = useState(localStorage.getItem("access_token") ? jwt_decode(localStorage.getItem("access_token")) : null);
+
 	return (
 		<>
-			<Header />
+			<Header user={user} auth={auth} />
 			<FirstPart />
 			<Cards />
 		</>
 	);
 };
 
-export default mainPage;
+export default MainPage;
